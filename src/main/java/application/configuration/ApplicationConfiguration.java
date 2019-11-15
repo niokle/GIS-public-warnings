5034b0a6784f1750e817ad6357aa4e51db11762d
niokle <myszapl@gmail.com>
2019-11-15 22:11:07 +0100

    configuration and scheduler classes added

---
+++ src/main/java/application/configuration/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package application.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class ApplicationConfiguration {
+}
---
+++ src/main/java/application/scheduler/ApplicationScheduler.java
@@ -0,0 +1,16 @@
+package application.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationScheduler {
+    private static Logger LOGGER = LoggerFactory.getLogger(ApplicationScheduler.class);
+
+    @Scheduled(cron = "*/2 * * * * *")
+    public void test() {
+        LOGGER.info("Uruchomiono zadanie schedulera");
+    }
+}
